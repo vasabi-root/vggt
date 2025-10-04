e92c192dac1a8e206b5b150e1c3a68c03e159ede
bashmac.wsl <vasabi.ph@gmail.com>
2025-10-04 22:57:57 +0300

    Sort images by name before any processing to preserve the right order

--- demo_colmap.py
+++ demo_colmap.py
@@ -120,6 +120,7 @@ def demo_fn(args):
     # Get image paths and preprocess them
     image_dir = os.path.join(args.scene_dir, "images")
     image_path_list = glob.glob(os.path.join(image_dir, "*"))
+    image_path_list.sort()
     if len(image_path_list) == 0:
         raise ValueError(f"No images found in {image_dir}")
     base_image_path_list = [os.path.basename(path) for path in image_path_list]
